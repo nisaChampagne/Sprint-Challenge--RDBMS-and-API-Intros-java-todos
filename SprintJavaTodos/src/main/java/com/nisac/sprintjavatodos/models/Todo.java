39664d2d650c3472b00c655c3134941bb1d34d00
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-09-13 11:48:57 -0400

    todo model done

--- SprintJavaTodos/src/main/java/com/nisac/sprintjavatodos/models/Todo.java
+++ SprintJavaTodos/src/main/java/com/nisac/sprintjavatodos/models/Todo.java
@@ -1,9 +1,16 @@
 package com.nisac.sprintjavatodos.models;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "todo")
 public class Todo extends Auditable
 {
-
-    //TODO
 
     //todoid primary key, not null long
     //description string, not null
@@ -11,4 +18,72 @@ public class Todo extends Auditable
     //completed boolean
     //userid foreign key (one user to many todos) not null
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date datestarted;
+
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "userid", nullable = false)
+    @JsonIgnoreProperties("todos")
+    private User user;
+
+    public Todo()
+    {
+
+    }
+
+    public Todo(String description, Date datestarted, boolean completed, User user) {
+        this.description = description;
+        this.datestarted = datestarted;
+        this.completed = completed;
+        this.user = user;
+    }
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDatestarted() {
+        return datestarted;
+    }
+
+    public void setDatestarted(Date datestarted) {
+        this.datestarted = datestarted;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
