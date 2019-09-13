eb2c8ecd99d28d13fc9795025b312348ec683117
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-09-13 11:41:57 -0400

    roles model done

--- SprintJavaTodos/src/main/java/com/nisac/sprintjavatodos/models/Role.java
+++ SprintJavaTodos/src/main/java/com/nisac/sprintjavatodos/models/Role.java
@@ -1,5 +1,13 @@
 package com.nisac.sprintjavatodos.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
 public class Role extends  Auditable
 {
 
@@ -8,4 +16,48 @@ public class Role extends  Auditable
 //    roleid primary key, not null long
 //    rolename string not null unique
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long roleid;
+
+    @Column(nullable = false, unique = true)
+    String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("role")//to avoid loop
+    private List<UserRoles> userRoles = new ArrayList<>();
+
+    public Role()
+    {
+
+    }
+
+    public Role(String name, List<UserRoles> userRoles) {
+        this.name = name;
+        this.userRoles = userRoles;
+    }
+
+    public long getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(long roleid) {
+        this.roleid = roleid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<UserRoles> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoles> userRoles) {
+        this.userRoles = userRoles;
+    }
 }
