35bdbbe829ba6f54418b9d00354acf6f314fc030
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-09-13 11:59:14 -0400

    userroles model done

--- SprintJavaTodos/src/main/java/com/nisac/sprintjavatodos/models/UserRoles.java
+++ SprintJavaTodos/src/main/java/com/nisac/sprintjavatodos/models/UserRoles.java
@@ -1,6 +1,14 @@
 package com.nisac.sprintjavatodos.models;
 
-public class UserRoles extends Auditable
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name ="userroles")
+public class UserRoles extends Auditable implements Serializable
 {
 
 //    USERROLES
@@ -8,4 +16,46 @@ public class UserRoles extends Auditable
 //    roleid foreign key to role
 //    userid foreign key to user
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    @JsonIgnoreProperties("userRoles")
+    private User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ManyToOne
+    @JoinColumn(name ="roleid")
+    @JsonIgnoreProperties("userRoles")
+    private Role role;
+
+    public UserRoles()
+    {
+
+    }
+
+
+    public UserRoles(User user, Role role)
+    {
+        this.user = user;
+        this.role = role;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
